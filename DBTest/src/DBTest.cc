3cdf4295594818cbb50f39980901841d449a7d5c
liujy <liujy@liujy-Lenovo>
2014-11-18 19:42:57 +0800

    sinperptr problem

--- DBTest/src/DBTest.cc
+++ DBTest/src/DBTest.cc
@@ -3,7 +3,8 @@
 #include "SniperKernel/AlgFactory.h"
 #include "SniperKernel/SniperPtr.h"
 #include "DatabaseSvc/IQuery.h"
-
+#include "DatabaseSvc/MongoDB.h"
+//using namespace mongo;
 DECLARE_ALGORITHM(DBTest);
 
 DBTest::DBTest(const std::string& name)
@@ -19,14 +20,23 @@ DBTest::initialize() {
 
 bool
 DBTest::execute() {
-    SniperPtr<IQuery> query_svc(getScope(), "MyMongoDB");
-    IQuery::QueryString querystr = "";
-    IQuery::QueryResult results = query_svc->query(querystr);
-
-    std::cout << "+ Result: " << std::endl;
-    for (IQuery::QueryResult::iterator it = results.begin();
+    SniperPtr<MyMongoDB> query_svc(getScope(), "MyMongoDB");
+    IQuery::QueryString querystr1 = "";
+    IQuery::RecordString querystr2  = "{\"name\":\"lizjing\", \"age\": 25,\"num\":20110056}";
+    IQuery::QueryRecord results = query_svc->query(querystr1);
+    
+    LogInfo << "+ Result: " << std::endl;
+    for (IQuery::QueryRecord::iterator it = results.begin();
 	 it != results.end();++it) {
-      std::cout << (*it) << std::endl;
+      LogInfo << (*it) << std::endl;
+    }
+    LogInfo<<"!!!!!!"<<std::endl;
+    bool result;
+    
+    result = query_svc->insert(querystr2);
+
+    if (result) {
+        LogInfo << "insert success" << std::endl;
     }
 
     return true;
