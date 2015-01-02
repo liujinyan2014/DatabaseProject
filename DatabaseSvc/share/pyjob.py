50dae422709d6098ca925c60d9bc709458571580
Jinyan Liu <jyliu@ihep.ac.cn>
2015-01-02 17:08:17 +0800

    insert data.

--- DatabaseSvc/share/pyjob.py
+++ DatabaseSvc/share/pyjob.py
@@ -14,10 +14,34 @@ if __name__ == "__main__":
     Sniper.loadDll("libDatabaseSvc.so")
     import libDatabaseSvc
     dbsvc = task.createSvc("MyMongoDB")
+    dbsvc.initialize()
     print dbsvc
     # assume we can query directly
     record = libDatabaseSvc.QueryRecord();
     dbsvc.query("", record)
     import json
-    for r in  results:
+    for r in  record:
         print json.loads(r)
+
+    r = json.dumps({'x':'liu'})
+    print dbsvc.insert(r)
+
+    # load PMT theta and phi
+    import os
+    detroot = os.environ["DETSIMOPTIONSROOT"] 
+    cdpmt = os.path.join(detroot, "data",
+            "PMTPos_Acrylic.csv")
+    if (not os.path.exists(cdpmt)):
+        raise Exception("can't find the PMT file %s, please setup env."%cdpmt)
+    with open(cdpmt) as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            no, theta, phi = line.split()
+            print theta, phi
+            r = json.dumps({'theta':theta, 'phi':phi})
+            print dbsvc.insert(r)
+
+    # end
+    dbsvc.finalize()
